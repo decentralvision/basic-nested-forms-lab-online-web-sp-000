8c904b21f5619ff370309873cc13f7820a598d65
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-24 05:51:08 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients << Ingredient.new
+    @recipe.ingredients << Ingredient.new
   end
 
   def create
